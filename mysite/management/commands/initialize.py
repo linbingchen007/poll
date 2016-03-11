c5bb074d78516f76361bf3357ced70bcb759474e
linbingchen <linbingchen2012@gmail.com>
2016-03-11 10:24:22 +0800

    keep safe

--- mysite/management/commands/initialize.py
+++ mysite/management/commands/initialize.py
@@ -10,15 +10,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         #设定超时60秒 进行最终评判
-        Pic.objects.all().delete()
-        User.objects.all().delete()
-        Valid.objects.all().delete()
-        Question.objects.all().delete()
-        Var.objects.all().delete()
-        Candidate.objects.all().delete()
-        Exl.objects.all().delete()
-        Log.objects.all().delete()
-        Text.objects.all().delete()
+        #Pic.objects.all().delete()
+        #User.objects.all().delete()
+        #Valid.objects.all().delete()
+        #Question.objects.all().delete()
+        #Var.objects.all().delete()
+        #Candidate.objects.all().delete()
+        #Exl.objects.all().delete()
+        #Log.objects.all().delete()
+        #Text.objects.all().delete()
+        print "hehe"
         Var(name = "authkey", val = "10000").save()
         Var(name = "bind", val = "0").save()
         Var(name = "seed", val = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))).save()
